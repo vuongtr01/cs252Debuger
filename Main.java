0171cf47c9d998f70bd769cfd17232689aa6220a
ghazal Alabtah <alabgh01@luther.edu>
2021-11-20 15:34:29 -0600

    added file chooser and changed functionality of q button

--- Main.java
+++ Main.java
@@ -3,47 +3,69 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Scanner;
 import vm252utilities.VM252Utilities;
+import java.io.File;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 public class Main
 {
     public static void main(String [] commandLineArguments)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter VM252 object-file name: ");
-        String objectFileName = in.nextLine();
 
-        byte [] program = VM252Utilities.readObjectCodeFromObjectFile(objectFileName);
+        EventQueue.invokeLater(
+            () -> 
+            {
 
-        if (program == null)
-            System.out.println(
-                    "File does not exist or isn't a valid VM252 object-code file"
-                    );
-        else{
-            EventQueue.invokeLater(
-                () ->
-                    {
-
-
-                        //
-                        // Create program frame
-                        //
-
-                            ProgramFrame frame = new ProgramFrame(program);
-
-                        //
-                        // Set frame's visibility and closing behavior
-                        //
-
-                            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            frame.setVisible(true);
-
-                        }
-
-                );
-        }
+                // call the file chooser method from the file chooser class
+                ObjectFileChooser File = new ObjectFileChooser();
+                File.ObjectFileChooser();
+            }
+        );
 
     }
+
 }
+
+class ObjectFileChooser extends JFileChooser
+{
+    public void ObjectFileChooser()
+    {
+        // Create a new File chooser from JFileChooser()
+        JFileChooser fileChooser = new JFileChooser();
+
+        // Adding filter to the file chooser 
+        // to only allow choices of files of type vm252 obj
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Opcode Files", "vm252obj");
+        fileChooser.setFileFilter(filter);
+        
+        // Specify the location to be the current directory for the file chooser search
+        fileChooser.setCurrentDirectory(new File("."));
+
+        // assign the result from file choices to response
+        // it is int because it will be 0 if a file was chosen
+        // it will be 1 if no file was choosen, and user clicked x or cancel
+        int response = fileChooser.showOpenDialog(null);
+        
+        // if there was a file chosen to open,
+        // get the file name from file chooser
+        // get the object code from the choosen file
+        // then feed that into the program
+        if(response == JFileChooser.APPROVE_OPTION) { 
+            String file = fileChooser.getSelectedFile().getName();
+            byte [] program = VM252Utilities.readObjectCodeFromObjectFile(file);
+            ProgramFrame frame = new ProgramFrame(program);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        }
+        // if no file was choosen
+        // exit the program
+        else{
+            System.exit(0);
+        }
+    }   
+}
+
 
 class ProgramFrame extends JFrame
 {
@@ -74,6 +96,7 @@ class ProgramFrame extends JFrame
     //
     //Ctors
     //
+
 
     public ProgramFrame(byte[] program)
     {
